f74b9d4cc2e748f379a2c37da30eefcfaf826e7f
gustavogzr <gustavogzr@hotmail.com>
2022-09-07 17:33:33 -0300

    Ajustes

--- tabela_hash/Hash.cpp
+++ tabela_hash/Hash.cpp
@@ -1,4 +1,4 @@
-#include <Hash.h>
+#include <hash.h>
 #include <iostream>
 using namespace std;
 
---
+++ tabela_hash/hash_aplicacao.cpp
@@ -0,0 +1,27 @@
+/*  
+código para compilar o programa:
+g++ hash_aplicacao.cpp Aluno.cpp Hash.cpp -o hash_app1
+
+*/
+
+#include <hash.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    Hash alunosHash(10);
+    int ras[7] = {
+        12704, 31300, 1234,
+        49001, 52202, 65606,
+        91234};
+    string nomes[7] = {
+        "Pedro", "Raul", "Paulo",
+        "Carlos", "Lucas", "Maria",
+        "Samanta"};
+    for (int i = 0; i < 7; i++)
+    {
+        Aluno aluno = Aluno(ras[i],nomes[i]);
+        alunosHash.insertItem(aluno);
+    }
+    alunosHash.print();    
+}
