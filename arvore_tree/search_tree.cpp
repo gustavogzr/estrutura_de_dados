d2e9220f974463436a2b04cb923c0c6ce9795574
gustavogzr <gustavogzr@hotmail.com>
2022-09-19 22:46:30 -0300

    Aula 15

--- arvore_tree/search_tree.cpp
+++ arvore_tree/search_tree.cpp
@@ -47,6 +47,38 @@ void SearchTree::insertAluno(NodeType*& tree, Aluno aluno)  {
     else
         insertAluno(tree->direita, aluno);
 }
+void SearchTree::deleteAluno(NodeType*& tree, int aluno) {
+    if (aluno < tree->aluno.getRa())
+        deleteAluno(tree->esquerda, aluno);
+    else if (aluno > tree->aluno.getRa())
+        deleteAluno(tree->direita, aluno);
+    else if (aluno == tree->aluno.getRa())
+        deleteNode(tree);
+}
+void SearchTree::deleteNode(NodeType*& tree) {
+    Aluno data;
+    NodeType* tempPtr;
+    tempPtr = tree;
+    if (tree->esquerda == NULL) {
+        tree = tree->direita;
+        delete tempPtr;
+    }
+    else if (tree->direita == NULL) {
+        tree = tree->esquerda;
+        delete tempPtr;
+    }
+    else {
+        getSuccessor(tree, data);
+        tree->aluno = data;
+        deleteAluno(tree->direita, data.getRa());
+    }
+}
+void SearchTree::getSuccessor(NodeType* tree, Aluno& data) {
+    tree = tree->direita;
+    while (tree->esquerda != NULL)
+        tree = tree->esquerda;
+    data = tree->aluno;
+}
 void SearchTree::printPreOrder(NodeType *tree) const {
     if (tree != NULL) {
         std::cout << tree->aluno.getNome() << " , ";
