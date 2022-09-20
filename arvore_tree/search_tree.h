0058a458ff79f886b8f82ee37ffe5342574ffc2e
gustavogzr <gustavogzr@hotmail.com>
2022-09-19 22:13:03 -0300

    search_tree aula 14

--- arvore_tree/search_tree.h
+++ arvore_tree/search_tree.h
@@ -11,11 +11,11 @@ struct NodeType
     NodeType* direita;
 };
 
-class SeachTree
+class SearchTree
 {
 public:
-    SeachTree() {root = NULL;}
-    ~SeachTree() {destroyTree(root);}
+    SearchTree() {root = NULL;}
+    ~SearchTree() {destroyTree(root);}
     bool isEmpty() const;
     bool isFull() const;
     void retrieveAluno(Aluno& item, bool& found) const {
@@ -30,7 +30,7 @@ private:
     void destroyTree(NodeType*& tree);
     void retrieveAluno(NodeType* tree, Aluno& item, bool& found) const;
     void insertAluno(NodeType*& tree, Aluno item);
-    void deleteAluno(NodeType*& tree);
+    void deleteAluno(NodeType*& tree, int item);
     void deleteNode(NodeType*& tree);
 void getSuccessor(NodeType* tree, Aluno& data);
 void printTree(NodeType* tree) const;
@@ -38,7 +38,7 @@ void printPreOrder(NodeType* tree) const;
 void printInOrder(NodeType* tree) const;
 void printPostOrder(NodeType* tree) const;
 
-NodeType* root;
+NodeType* root; //RAIZ DA ARVORE BINARIA
 };
 
 
